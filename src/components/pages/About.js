b8e82b46072722cb1c0628bf8cef5e4deee935c5
afroz-in <kingkhanafroz517@gmail.com>
2020-09-06 00:11:18 +0530

    my project

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+    render() {
+        return (
+            <div>
+                <center>
+               <h1>About This App</h1>
+               <h3> Search Engine for Github Users</h3> 
+               <h4> Made By Afroz Khan</h4>
+               </center>
+            </div>
+        )
+    }
+}
+
+export default About
